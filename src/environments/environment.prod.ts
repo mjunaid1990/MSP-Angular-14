b5d8627d53c45eb6f15e12db2eeefbb90a988ffd
mjunaid1990 <m.junaid1990@hotmail.com>
2023-12-25 16:52:39 +0500

    Delete src/environments/environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  mykey: 'development',
-  staging: true,
-  liveUrl: 'https://live.marketsharepro.com/api/',
-  stagingUrl: 'https://staging.marketsharepro.com/api/'
-};
